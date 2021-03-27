3e1525b356291bd59f9aae54d11c06fefea4f74c
aakash4069 <aakash4069@gmail.com>
2021-03-28 00:29:56 +0530

    Script code

---
+++ Code/p.java
@@ -0,0 +1,15 @@
+public static Class p{
+
+    public static void main(String[] args){
+        Sanner scn=new Scanner(System.in);
+
+        String str=nextLine();
+        Ispil
+    
+    }
+    public static boolean Ispil(int n){
+
+        if()
+    }
+}
+
