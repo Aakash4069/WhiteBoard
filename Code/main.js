2e1f0df8ae8d963aa226974035bee862880fed2d
aakash4069 <aakash4069@gmail.com>
2021-03-28 00:30:55 +0530

    Dev_tools is off

--- Code/main.js
+++ Code/main.js
@@ -22,7 +22,7 @@ function createWindow() {
     win.loadFile("index.html").then(function () {
         // console.log("Window opened successfully");
         win.maximize();
-        win.webContents.openDevTools();
+        // win.webContents.openDevTools();
     })
 }
 app.whenReady().then(createWindow);
